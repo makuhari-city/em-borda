2c7416c5f7300d3310fcca571045846b2b5d744d
Yasushi Sakai <yasushisakai+git@pm.me>
2021-07-02 07:28:02 -0400

    Data format adjustment, fixed calculation.
    Data format is consistant for the new `VoteInfo` and `Topic`.
    The results were different when we tried this out in Tuesday,
    fixed it. Now the calculations meet the tiny experiment.

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,46 @@
 use std::collections::HashMap;
 use uuid::Uuid;
-use vote::{Policy, TopicInfo};
+use vote::VoteInfo;
 
 type BordaResult = Vec<(Uuid, u32)>;
 
-pub fn calculate(info: TopicInfo) -> BordaResult {
+/// I initially thought that votes casted values with 0 should be treated
+/// as a white ballot (not voting), but this slight change will change the bahaviour of the
+/// calculation....
+/// If people do not give any number to a policy, the map will not contain that entry. In this type
+/// of borda count, those entries will be 'counted down', in other terms, the borda count value
+/// will be 0.
+pub async fn calculate(info: VoteInfo) -> BordaResult {
     let mut result: HashMap<Uuid, u32> = info
         .policies
         .iter()
-        .map(|(uid, _title)| (uid.to_owned(), 0u32))
+        .map(|uid| (uid.to_owned(), 0u32))
         .collect();
 
     let stripped = info.only_policy_voting();
 
-    for (_, vote) in stripped {
+    let vote_vec = stripped
+        .iter()
+        .map(|(_, vote)| {
+            vote.iter()
+                .map(|(u, v)| (u.to_owned(), *v))
+                .collect::<Vec<(Uuid, f64)>>()
+        })
+        .collect::<Vec<Vec<(Uuid, f64)>>>();
+
+    // TODO: I can parrallize this, sorting and putting point can be done asyncly
+    for vote in vote_vec {
         // first sort the votes
-        vote.to_owned()
-            .sort_by(|(_ida, a), (_idb, b)| a.partial_cmp(b).unwrap());
+        let mut sorted = vote.to_owned();
+        sorted.sort_by(|(_ida, a), (_idb, b)| b.partial_cmp(a).unwrap());
 
         // let mut order: Vec<&Uuid> = vote.iter().map(|(uid, _)| uid).collect();
-        let order: Vec<&Uuid> = vote.iter().map(|(uid, _)| uid).collect();
+        let order: Vec<&Uuid> = sorted.iter().map(|(uid, _)| uid).collect();
 
-        // this part considers vote trucatenation:
-        // we choose Borda's original implementation, we round-down the policies that are not
-        // listed in the voters vote
-
-        /*
-        let mut redacted: Vec<Policy> = info
-            .policies
-            .iter()
-            .filter(|(uid, _)| vote.iter().any(|(v, _)| v == uid))
-            .cloned()
-            .collect();
-
-        redacted.sort_by_key(|(uid, _title)| uid.to_string()); // random but determistic by uuid
-
-        let mut not_voted: Vec<&Uuid> = redacted.iter().map(|(uid, _)| uid).collect();
-
-        order.append(&mut not_voted);
-        */
+        println!("{:?}", &order);
 
         for (i, uid) in order.iter().enumerate() {
-            let point = (order.len() - i) as u32;
+            let point = (info.policies.len() - i) as u32;
             let current = result.get_mut(uid).expect("unknown policy found in vote");
             *current += point;
         }
@@ -55,294 +54,159 @@ pub fn calculate(info: TopicInfo) -> BordaResult {
 }
 
 #[cfg(test)]
-mod fptp_test {
+mod borda_test {
 
     use super::*;
 
-    #[test]
-    fn simple() {
+    #[actix_rt::test]
+    async fn simple() {
         let json_data = br#"{
-  "title": "topic title",
-  "id": "60556c87-9af2-4e57-bf63-82ca6123082c",
-  "votes": {
-    "046c12e1-906a-492f-8614-39dfa87d676d": [
-      [
-        "0f18b644-3789-4194-9a98-0e08040395b7",
-        1
-      ]
-    ],
-    "cc652ec5-0a11-48da-9189-4642473bb54e": [
-      [
-        "0f18b644-3789-4194-9a98-0e08040395b7",
-        1
-      ]
-    ],
-    "2c8c6db4-ba7c-48dd-8eaa-f4ce886dc0e9": [
-      [
-        "a076bf38-55b3-42c0-8cd5-d89381152e10",
-        1
-      ]
-    ]
+    "votes": {
+    "046c12e1-906a-492f-8614-39dfa87d676d": {
+        "0f18b644-3789-4194-9a98-0e08040395b7":1
+    },
+    "cc652ec5-0a11-48da-9189-4642473bb54e": {
+    "0f18b644-3789-4194-9a98-0e08040395b7": 1.0
+      },
+    "2c8c6db4-ba7c-48dd-8eaa-f4ce886dc0e9": {
+        "a076bf38-55b3-42c0-8cd5-d89381152e10":1
+      }
   },
   "delegates": [
-    [
       "2c8c6db4-ba7c-48dd-8eaa-f4ce886dc0e9",
-      "alice"
-    ],
-    [
       "046c12e1-906a-492f-8614-39dfa87d676d",
-      "bob"
-    ],
-    [
-      "cc652ec5-0a11-48da-9189-4642473bb54e",
-      "charlie"
-    ]
+      "cc652ec5-0a11-48da-9189-4642473bb54e"
   ],
   "policies": [
-    [
       "a076bf38-55b3-42c0-8cd5-d89381152e10",
-      "apples"
-    ],
-    [
       "0f18b644-3789-4194-9a98-0e08040395b7",
-      "bananas"
-    ],
-    [
-      "55bac309-5534-4e01-a5fc-7eae2b2d818e",
-      "orange"
-    ]
+      "55bac309-5534-4e01-a5fc-7eae2b2d818e"
   ]
 }
 "#;
 
-        let info: TopicInfo = serde_json::from_slice(json_data).unwrap();
-        let result = calculate(info);
+        let info: VoteInfo = serde_json::from_slice(json_data).unwrap();
+        let result = calculate(info).await;
         let winner: Uuid = Uuid::parse_str("0f18b644-3789-4194-9a98-0e08040395b7").unwrap();
         assert_eq!(result[0].0, winner);
     }
-    #[test]
-    fn dont_include_delegates() {
+    #[actix_rt::test]
+    async fn dont_include_delegates() {
         let json_data = br#"{
-  "title": "topic title",
-  "id": "60556c87-9af2-4e57-bf63-82ca6123082c",
   "votes": {
-    "046c12e1-906a-492f-8614-39dfa87d676d": [
-      [
-        "0f18b644-3789-4194-9a98-0e08040395b7",
-        1
-      ]
-    ],
-    "cc652ec5-0a11-48da-9189-4642473bb54e": [
-      [
-        "0f18b644-3789-4194-9a98-0e08040395b7",
-        1
-      ],
-      [
-        "046c12e1-906a-492f-8614-39dfa87d676d",
-        1
-      ]
-
-    ],
-    "2c8c6db4-ba7c-48dd-8eaa-f4ce886dc0e9": [
-      [
-        "a076bf38-55b3-42c0-8cd5-d89381152e10",
-        1
-      ],
-      [
-        "046c12e1-906a-492f-8614-39dfa87d676d",
-        1
-      ]
-
-    ]
+    "046c12e1-906a-492f-8614-39dfa87d676d": {
+        "0f18b644-3789-4194-9a98-0e08040395b7":1
+    },
+    "cc652ec5-0a11-48da-9189-4642473bb54e": {
+        "0f18b644-3789-4194-9a98-0e08040395b7":1,
+        "046c12e1-906a-492f-8614-39dfa87d676d":1
+    },
+    "2c8c6db4-ba7c-48dd-8eaa-f4ce886dc0e9": {
+        "a076bf38-55b3-42c0-8cd5-d89381152e10":1,
+        "046c12e1-906a-492f-8614-39dfa87d676d":1
+    }
   },
   "delegates": [
-    [
       "2c8c6db4-ba7c-48dd-8eaa-f4ce886dc0e9",
-      "alice"
-    ],
-    [
       "046c12e1-906a-492f-8614-39dfa87d676d",
-      "bob"
-    ],
-    [
-      "cc652ec5-0a11-48da-9189-4642473bb54e",
-      "charlie"
-    ]
+      "cc652ec5-0a11-48da-9189-4642473bb54e"
   ],
   "policies": [
-    [
       "a076bf38-55b3-42c0-8cd5-d89381152e10",
-      "apples"
-    ],
-    [
       "0f18b644-3789-4194-9a98-0e08040395b7",
-      "bananas"
-    ],
-    [
-      "55bac309-5534-4e01-a5fc-7eae2b2d818e",
-      "orange"
-    ]
+      "55bac309-5534-4e01-a5fc-7eae2b2d818e"
   ]
 }
 "#;
 
-        let info: TopicInfo = serde_json::from_slice(json_data).unwrap();
-        let result = calculate(info);
+        let info: VoteInfo = serde_json::from_slice(json_data).unwrap();
+        let result = calculate(info).await;
         let winner: Uuid = Uuid::parse_str("0f18b644-3789-4194-9a98-0e08040395b7").unwrap();
         assert_eq!(result[0].0, winner);
     }
-    #[test]
-    fn multiple() {
+    #[actix_rt::test]
+    async fn multiple() {
         let json_data = br#"{
-  "title": "topic title",
-  "id": "60556c87-9af2-4e57-bf63-82ca6123082c",
-  "votes": {
-    "046c12e1-906a-492f-8614-39dfa87d676d": [
-      [
-        "0f18b644-3789-4194-9a98-0e08040395b7",
-        0.8
-      ],
-      [
-        "a076bf38-55b3-42c0-8cd5-d89381152e10",
-        0.7
-      ]
-    ],
-    "cc652ec5-0a11-48da-9189-4642473bb54e": [
-      [
-        "0f18b644-3789-4194-9a98-0e08040395b7",
-        1
-      ]
-    ],
-    "2c8c6db4-ba7c-48dd-8eaa-f4ce886dc0e9": [
-      [
-        "a076bf38-55b3-42c0-8cd5-d89381152e10",
-        1
-      ]
-    ]
+   "votes": {
+    "046c12e1-906a-492f-8614-39dfa87d676d": {
+        "0f18b644-3789-4194-9a98-0e08040395b7":0.8,
+        "a076bf38-55b3-42c0-8cd5-d89381152e10":0.7
+    },
+    "cc652ec5-0a11-48da-9189-4642473bb54e": {
+        "0f18b644-3789-4194-9a98-0e08040395b7": 1
+    },
+    "2c8c6db4-ba7c-48dd-8eaa-f4ce886dc0e9": {
+        "a076bf38-55b3-42c0-8cd5-d89381152e10": 1
+    }
   },
   "delegates": [
-    [
       "2c8c6db4-ba7c-48dd-8eaa-f4ce886dc0e9",
-      "alice"
-    ],
-    [
       "046c12e1-906a-492f-8614-39dfa87d676d",
-      "bob"
-    ],
-    [
-      "cc652ec5-0a11-48da-9189-4642473bb54e",
-      "charlie"
-    ]
+      "cc652ec5-0a11-48da-9189-4642473bb54e"
   ],
   "policies": [
-    [
       "a076bf38-55b3-42c0-8cd5-d89381152e10",
-      "apples"
-    ],
-    [
       "0f18b644-3789-4194-9a98-0e08040395b7",
-      "bananas"
-    ],
-    [
-      "55bac309-5534-4e01-a5fc-7eae2b2d818e",
-      "orange"
-    ]
+      "55bac309-5534-4e01-a5fc-7eae2b2d818e"
   ]
 }
 "#;
 
-        let info: TopicInfo = serde_json::from_slice(json_data).unwrap();
-        let result = calculate(info);
+        let info: VoteInfo = serde_json::from_slice(json_data).unwrap();
+        let result = calculate(info).await;
         let winner: Uuid = Uuid::parse_str("0f18b644-3789-4194-9a98-0e08040395b7").unwrap();
         assert_eq!(result[0].0, winner);
     }
 
-    #[test]
-    fn empty() {
+    #[actix_rt::test]
+    async fn empty() {
         let json_data = br#"{
 
   "title": "topic title",
   "id": "60556c87-9af2-4e57-bf63-82ca6123082c",
   "votes": {},
-  "delegates": [
-    [
+  "delegates":[
       "2c8c6db4-ba7c-48dd-8eaa-f4ce886dc0e9",
-      "alice"
-    ],
-    [
       "046c12e1-906a-492f-8614-39dfa87d676d",
-      "bob"
-    ],
-    [
-      "cc652ec5-0a11-48da-9189-4642473bb54e",
-      "charlie"
-    ]
+      "cc652ec5-0a11-48da-9189-4642473bb54e"
   ],
   "policies": [
-    [
       "a076bf38-55b3-42c0-8cd5-d89381152e10",
-      "apples"
-    ],
-    [
       "0f18b644-3789-4194-9a98-0e08040395b7",
-      "bananas"
-    ],
-    [
-      "55bac309-5534-4e01-a5fc-7eae2b2d818e",
-      "orange"
-    ]
+      "55bac309-5534-4e01-a5fc-7eae2b2d818e"
   ]
 }
 "#;
 
-        let info: TopicInfo = serde_json::from_slice(json_data).unwrap();
-        let result = calculate(info);
+        let info: VoteInfo = serde_json::from_slice(json_data).unwrap();
+        let result = calculate(info).await;
         assert_eq!(result[0].1, result[1].1);
     }
-    #[test]
-    fn empty_delegates() {
+    #[actix_rt::test]
+    async fn empty_delegates() {
         let json_data = br#"{
-
   "title": "topic title",
   "id": "60556c87-9af2-4e57-bf63-82ca6123082c",
   "votes": {
-    "2c8c6db4-ba7c-48dd-8eaa-f4ce886dc0e9":[
-        ["cc652ec5-0a11-48da-9189-4642473bb54e", 1.0]
-    ]
+    "2c8c6db4-ba7c-48dd-8eaa-f4ce886dc0e9":{
+        "cc652ec5-0a11-48da-9189-4642473bb54e":1.0
+    }
   },
   "delegates": [
-    [
       "2c8c6db4-ba7c-48dd-8eaa-f4ce886dc0e9",
-      "alice"
-    ],
-    [
       "046c12e1-906a-492f-8614-39dfa87d676d",
-      "bob"
-    ],
-    [
-      "cc652ec5-0a11-48da-9189-4642473bb54e",
-      "charlie"
-    ]
+      "cc652ec5-0a11-48da-9189-4642473bb54e"
   ],
   "policies": [
-    [
       "a076bf38-55b3-42c0-8cd5-d89381152e10",
-      "apples"
-    ],
-    [
       "0f18b644-3789-4194-9a98-0e08040395b7",
-      "bananas"
-    ],
-    [
-      "55bac309-5534-4e01-a5fc-7eae2b2d818e",
-      "orange"
-    ]
+      "55bac309-5534-4e01-a5fc-7eae2b2d818e"
   ]
 }
 "#;
 
-        let info: TopicInfo = serde_json::from_slice(json_data).unwrap();
-        let result = calculate(info);
+        let info: VoteInfo = serde_json::from_slice(json_data).unwrap();
+        let result = calculate(info).await;
         assert_eq!(result[0].1, result[1].1);
     }
 }
